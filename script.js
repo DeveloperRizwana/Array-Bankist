7df7d74d2ac883bf02fbd7269fda993ba24a1bd7
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-05-08 08:47:06 +0530

    learned to create username

--- script.js
+++ script.js
@@ -6,28 +6,28 @@
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: 'Rizwana Perween',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
+  owner: 'Noorjahan Khatoon',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
+  owner: 'Md Islam',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
 };
 
 const account4 = {
-  owner: 'Sarah Smith',
+  owner: 'Aatif Alam',
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
@@ -83,7 +83,18 @@ const displayMovements = function (movements) {
 
 displayMovements(account1.movements);
 
-
+const createUserName = function (accs) {
+  accs.forEach(function (acc) {
+    //Isse har account object mein ek userName property add ho jaayegi, jisme owner ke naam ke initials honge
+    acc.userName = acc.owner 
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+createUserName(accounts);
+console.log(accounts);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
