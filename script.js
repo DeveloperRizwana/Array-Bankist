1853e9151a66cd91775bbaa6ebaa5d281ca75ad0
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-05-12 10:18:00 +0530

    modified

--- script.js
+++ script.js
@@ -126,6 +126,7 @@ console.log(arr.slice(-2));
 console.log(arr.slice(-1));
 console.log(arr.slice(1, -1));
 console.log(arr.slice());
+
 console.log([...arr]);
 
 // SPLICE METHOD -- Destructive (Modifies the original array)
