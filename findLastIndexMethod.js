8eb67fbeb85d544e9a5a7e4c903291ce4128e664
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-05-14 12:50:37 +0530

    learned about the findlast and findlastIndex method

---
+++ findLastIndexMethod.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// FindLast method
+const lastWithdrawal = movements.findLast(mov => mov < 0);
+console.log(movements);
+console.log(lastWithdrawal);
+
+// FindlastIndex method
+const latestLargeMovementIndex = movements.findLastIndex(
+  mov => Math.abs(mov) > 1000
+);
+console.log(latestLargeMovementIndex);
+console.log(
+  `Your latest large movement was ${
+    movements.length - latestLargeMovementIndex
+  } movements ago`
+);
