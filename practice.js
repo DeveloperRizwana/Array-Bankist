91a7ab0a15a39707dc620bd73b9e5240cc5e0ccb
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-05-18 12:37:27 +0530

    practiced

--- practice.js
+++ practice.js
@@ -59,4 +59,35 @@ let a = 10;
 console.log(++a);
 
 const nm = ['R', 'i', 'z', 'w', 'a', 'n', 'a'];
-console.log(nm.reduce((acc,cur) => acc+cur));
+console.log(nm.reduce((acc, cur) => acc + cur));
+
+// Excercise - 3
+const sums = accounts
+  .flatMap(cur => cur.movements)
+  .reduce(
+    (sum, cur) => {
+      //   cur > 0 ? (sum.deposits += cur) : (sum.withdrawals += cur);
+      sum[cur > 0 ? 'deposits' : 'withdrawals'] += cur;
+      return sum;
+    },
+    { deposits: 0, withdrawals: 0 }
+  );
+console.log(sums);
+
+// Excercise - 4
+// this is a nice title --> This Is a Nice Title
+const convertTitleCase = function (title) {
+  const capitalized = str => str[0].toUpperCase() + str.slice(1);
+  const exception = ['a', 'an', 'the', 'with', 'and', 'but', 'or', 'in', 'on'];
+  const titleCase = title
+    .toLowerCase()
+    .split(' ') // Splits the string into an array of words.
+    .map(cur => (exception.includes(cur) ? cur : capitalized(cur))) // Slice --> Takes the rest of the word (excluding the first letter).
+    .join(' '); // Joins the modified words back into a single string.
+
+  //   return titleCase;
+  return capitalized(titleCase);
+};
+console.log(convertTitleCase(`this is a nice title`));
+console.log(convertTitleCase(`this is a LONG title but not too long`));
+console.log(convertTitleCase(`and here is another title with an EXAMPLE`));
