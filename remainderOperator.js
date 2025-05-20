8d30fcf97434bde53d8a898dd149ac8c5d8c77bf
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-05-20 12:33:00 +0530

    learned numeric seperator

--- remainderOperator.js
+++ remainderOperator.js
@@ -16,3 +16,25 @@ const isEven = n => n % 2 === 0;
 console.log(isEven(8));
 console.log(isEven(7));
 console.log(isEven(1332));
+
+// Numeric seperator
+// 287,460,000,000
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const price = 345_99;
+console.log(price);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.1415;
+console.log(PI);
+
+// ----->> we can't do this <-------
+// const PI = 3._1415;
+// const PI = 3.1__415;
+// const PI = 3.1415_;
+// const PI = _3.1415;
+console.log(parseInt('23_000'));
+console.log(Number('23_000'));
