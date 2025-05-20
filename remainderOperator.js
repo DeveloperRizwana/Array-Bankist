ef5275b479067884c584c2cd151d85830bb56899
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-05-20 12:04:54 +0530

    learned remainder operator in js

---
+++ remainderOperator.js
@@ -0,0 +1,18 @@
+'use strict';
+
+console.log(5 % 2);
+console.log(5 / 2); //5 = 2 * 2 + 1
+
+console.log(8 % 2);
+console.log(8 / 2); //8 = 2*3 + 2
+
+console.log(6 % 2); // even number
+console.log(6 / 2);
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(7));
+console.log(isEven(1332));
